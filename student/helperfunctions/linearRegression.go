4a54cd41af0eddd2c3cb4e7df67b7d3340d1754e
somulo <mcomulosammy37@gmail.com>
2024-10-31 10:58:27 +0300

    init: add file to calculate linear regression

---
+++ student/helperfunctions/linearRegression.go
@@ -0,0 +1,19 @@
+package helperfunctions
+
+//import "fmt"
+
+func LinearRg(x, y []float64) (float64, float64) {
+	// y = mx + c
+	var numerator float64
+	var denominator float64
+	meany := Average(y)
+	meanx := Average(x)
+
+	for i := 0; i < len(x); i++ {
+		numerator += (x[i] - meanx) * (y[i] - meany)
+		denominator += (x[i] - meanx) * (x[i] - meanx)
+	}
+	m := numerator / denominator
+	a := meany - m*meanx
+	return m, a
+}
